c46f792d86495371f1505696f835f0bba9ea81c6
BedzieJazdaElo <125198635+BedzieJazdaElo@users.noreply.github.com>
2023-04-13 16:56:34 +0200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,34 @@
 
 int orzel;
 
-int main() {
-  int orzel;
-  std::cout << "orzeł";
-  static_assert(1 == 2, "niedobry orzel");
-  // to jest górny orzeł
+int main()
+{
+    int orzel;
+    std::cout << "orzeł";
+    static_assert(1 == 2, "niedobry orzel");
+    // to jest górny orzeł
 
-  int pawel = 1;
-  float monia = 3.14;
-  int gruzio = static_cast<int>(monia); // lub int gruzio=monia;
-  int gruzia = static_cast<int>(monia); // tu już tylko tym
+    int pawel = 1;
+    float monia = 3.14;
+    int gruzio = static_cast<int>(monia); // lub int gruzio=monia;
+    int gruzia = static_cast<int>(monia); // tu już tylko tym
 
-  // dynamic_cast//to samo co wyżej tylko też w czasie programu
+    // dynamic_cast//to samo co wyżej tylko też w czasie programu
 
-  // const_cast//wskazniki do wartości stałych itp
-  // reinterpret_cast//rózne wskazniki do roznych wskazników
+    // const_cast//wskazniki do wartości stałych itp
+    // reinterpret_cast//rózne wskazniki do roznych wskazników
 
-  std::vector<int> wektor{1, 2, 3, 4};
-  for (auto element : wektor) {
-    std::cout << element << '\n';
-  }
+    std::vector<int> wektor{1, 2, 3, 4};
+    for (auto element : wektor)
+    {
+        std::cout << element << '\n';
+    }
 
-  auto orzelowa_funkcja(int kruczek, float pluczek)->decltype(kruczek);
-  // funkcja w inny sposób napisania i bardziej służy
-  // do szablonów funkcji (ogółem to potem)
+    auto orzelowa_funkcja(int kruczek, float pluczek)->decltype(kruczek);
+    // funkcja w inny sposób napisania i bardziej służy
+    // do szablonów funkcji (ogółem to potem)
 
-  // i tak se moge zmieniać
+    // i tak se moge zmieniać
+
+    // alignas do wyrównywania adresów
 }
