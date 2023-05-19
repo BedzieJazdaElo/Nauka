cf2c1b866ff11d69f67869cf76f12c7c98bb3025
BedzieJazdaElo <125198635+BedzieJazdaElo@users.noreply.github.com>
2023-05-19 21:18:43 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,17 +1,7 @@
-#include <iostream>
-#include <stdio.h>
-
-void f(char** namePtr){
-    std::cout<<&namePtr<<std::endl; // wyświetla adres wskaźnika 'namePtr' w funkcji 'f' 
-    std::cout<<namePtr<<std::endl; // wyświetla adres oryginalnej zmiennej 'name' w funkcji 'main'
-    std::cout<<*namePtr<<std::endl; // wyświetla ciąg znaków, na który wskazuje 'name'
-    (*namePtr)+=2; // zwiększa wskaźnik 'name'
-    // wyświetl mi adres zmiennej 'name' w funkcji 'main' za pomoca funkcji 'printf'
-}
+#include<iostream>
 
 int main(){
-    char* name="John";
-    std::cout<<&name<<std::endl; // wyświetla adres zmiennej 'name' w funkcji 'main'
-    f(&name); // wywołuje funkcję 'f' z adresem 'name' jako argumentem
-    std::cout<<&name<<std::endl; // wyświetla adres zmiennej 'name' w funkcji 'main'
+    char name[4]{ 'A', 'B', 'C', '\0'};
+    std::cout<<"Adres: "<<static_cast<void *>(&name)<<std::endl;
+    std::cout<<"Wartosc: "<<name<<std::endl;
 }
